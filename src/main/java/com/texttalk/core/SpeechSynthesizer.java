57ba31847f246ed02cc0db077e44eacb14f375b2
Andrew Boss <andrew@smartdeveloping.com>
2014-06-15 21:04:42 +0100

    Initial implementation of text processing and synthesis execution

---
+++ src/main/java/com/texttalk/core/SpeechSynthesizer.java
@@ -0,0 +1,7 @@
+package com.texttalk.core;
+
+/**
+ * Created by Andrew on 15/06/2014.
+ */
+public class SpeechSynthesizer {
+}
---
+++ src/main/java/com/texttalk/distrib/SpeechSynthesisBolt.java
@@ -0,0 +1,7 @@
+package com.texttalk.distrib;
+
+/**
+ * Created by Andrew on 15/06/2014.
+ */
+public class SpeechSynthesisBolt {
+}
