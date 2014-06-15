45f8590d6695f645a9712e65695e404ab96b5846
Andrew Boss <andrew@smartdeveloping.com>
2014-06-15 18:17:02 +0100

    Initial commit

---
+++ src/main/java/com/texttalk/core/TextProcessor.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2014 www.text-talk.com
+ * User: andrewboss
+ * Date: 04/06/2014
+ * Time: 22:16
+ */
+
+package com.texttalk.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TextProcessor {
+
+    private static final String WORD_SEPARATOR = " ";
+
+    public static List<String> splitText(String text, int maxLength) {
+
+        // clear multiple spaces and split into words by space
+        String[] words = text.trim().replaceAll("\\s+", " ").split(WORD_SEPARATOR);
+        List<String> splittedText = new ArrayList<String>();
+        StringBuilder line = new StringBuilder();
+        int lineLen = 0;
+        int wordCount = 0;
+
+
+        for (String word : words) {
+            wordCount++;
+            if (lineLen + (WORD_SEPARATOR + word).length() > maxLength) {
+                if (wordCount > 0) {
+                    splittedText.add(line.toString());
+                    line = new StringBuilder();
+                }
+                lineLen = 0;
+            }
+
+            word += WORD_SEPARATOR;
+            line.append(word);
+            lineLen += word.length();
+        }
+
+        if(line.length() > 0) splittedText.add(line.toString());
+
+        return splittedText;
+    }
+}
---
+++ src/main/test/java/com/texttalk/core/TextProcessorTest.java
@@ -0,0 +1,56 @@
+package com.texttalk.core;
+
+import java.util.List;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.testng.Assert.*;
+
+public class TextProcessorTest {
+
+    private final static Logger logger = LoggerFactory.getLogger(TextProcessorTest.class);
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testTextSplit() {
+        List<String> splittedText = TextProcessor.splitText("Aaaa      bbbbb   cccccccc asashdgadxxxxxxxxxssssssssssswwwwwwwwwww jashd llaksjd klaksjd klla 111 222 333",
+                20
+        );
+
+        logger.debug("List size: " + splittedText.size());
+
+        for(String text: splittedText) {
+            logger.debug("'" + text + "'");
+        }
+    }
+
+    @Test
+    public void testTextSplit2() {
+        List<String> splittedText = TextProcessor.splitText("Leidinys rengiamas Nacionalinės bibliografijos duomenų " +
+                "banko (NBDB) bei LNB elektroninio katalogo pagrindu. Medžiaga grupuojama temomis, o jų viduje - " +
+                "abėcėlės tvarka išdėstyti knygų (atskirti žvaigždutėmis), serialinių leidinių ir jų sudedamųjų dalių " +
+                "bibliografiniai įrašai, pabaigoje pateikiamos nuorodos. Turinyje prie temos pavadinimo nurodomi " +
+                "bibliografinių įrašų numeriai. Rodyklės pabaigoje pridėtas sąrašas leidinių, kurių straipsniai " +
+                "užregistruoti, asmenvardžių rodyklė ir skelbiami leidinių bibliotekininkystės klausimais interneto adresai.",
+                50
+        );
+
+        logger.debug("List size: " + splittedText.size());
+
+        for(String text: splittedText) {
+            logger.debug("'" + text + "'");
+        }
+    }
+}
